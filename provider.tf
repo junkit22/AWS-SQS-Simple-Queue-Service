3b6626987789b31da4733987a76508ca3da1217c
junkit22 <junkit22@gmail.com>
2024-07-27 11:14:39 +0800

    update

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -11,6 +11,7 @@ terraform {
 #Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  profile = "clean_up"
   # Edit below
   #access_key = "my-access-key"
   #secret_key = "my-secret-key"
