1b800d2f4652ace448172ac2895fb073d882b091
junkit22 <junkit22@gmail.com>
2024-07-27 11:11:58 +0800

    update

--- service-integration.tf
+++ service-integration.tf
@@ -14,26 +14,3 @@ resource "aws_sqs_queue" "terraform_queue" {
     Environment = "production"
   }
 }
-
-
-resource "aws_sqs_queue" "terraform_queue" {
-  name = "terraform-example-queue"
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-    maxReceiveCount     = 4
-  })
-}
-
-resource "aws_sqs_queue" "terraform_queue_deadletter" {
-  name = "terraform-example-deadletter-queue"
-}
-
-resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
-  queue_url = aws_sqs_queue.terraform_queue_deadletter.id
-
-  redrive_allow_policy = jsonencode({
-    redrivePermission = "byQueue",
-    sourceQueueArns   = [aws_sqs_queue.terraform_queue.arn]
-  })
-}
