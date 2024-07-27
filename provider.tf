63e80ed111740ec744b25f5772b8124160839c60
junkit22 <junkit22@gmail.com>
2024-07-27 11:16:05 +0800

    update

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 #Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  profile = "clean_up"
+ 
   # Edit below
   #access_key = "my-access-key"
   #secret_key = "my-secret-key"
