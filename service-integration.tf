8e616be1c7147eadfaa8140adff46f77b62bb107
junkit22 <junkit22@gmail.com>
2024-07-27 11:19:20 +0800

    DLQ created

--- service-integration.tf
+++ service-integration.tf
@@ -4,13 +4,27 @@ resource "aws_sqs_queue" "terraform_queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  #redrive_policy = jsonencode({
-   # deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-    #maxReceiveCount     = 4
-  #})
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
 
 
   tags = {
     Environment = "production"
   }
+}
+
+#Dead Letter Queue
+resource "aws_sqs_queue" "terraform_queue_deadletter" {
+  name = "terraform-junjie-tf-deadletter-queue"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.terraform_queue_deadletter.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.terraform_queue.arn]
+  })
 }
